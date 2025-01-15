e8d13d12ea59e136d281aef7c26046caf728a75b
hee <heheeun13@gmail.com>
2025-01-16 03:44:25 +0900

    [Feat/#39] Onboarding Confirm Bar 생성

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Bar/OnboardingConfirmBar.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Bar/OnboardingConfirmBar.swift
@@ -5,3 +5,35 @@
 //  Created by 김희은 on 1/16/25.
 //
 
+import SwiftUI
+
+struct OnboardingConfirmBar: View {
+    let grayButtonText: String
+    let orangeButtonText: String
+    var onTap: (() -> Void)?
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Button(action: {
+                onTap?()
+            }) {
+                Text(grayButtonText)
+                    .pretendardFont(.title2_sb_18)
+                    .padding(16)
+                    .foregroundStyle(.grayWhite)
+                    .background(
+                        RoundedRectangle(cornerRadius: 6)
+                            .fill(.gray09)
+                    )
+            }
+            BasicButton(text: orangeButtonText, onTap: onTap)
+        }
+        .padding(.vertical, 20)
+        .padding(.horizontal, 16)
+        .background(.grayWhite)
+    }
+}
+
+#Preview {
+    OnboardingConfirmBar(grayButtonText: "시간표 변경", orangeButtonText: "가입 완료", onTap: nil)
+}
