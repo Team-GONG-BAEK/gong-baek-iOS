9f08535d052697c115781bb53bb323a94c5cfea6
kms0233 <cnblue789@naver.com>
2025-01-16 03:52:50 +0900

    [Feat/#29] PersonBox 컴포넌트 구현

---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/PersonBox.swift
@@ -0,0 +1,52 @@
+//
+//  PersonBox.swift
+//  Gongbaek_iOS
+//
+//  Created by 김민서 on 1/16/25.
+//
+
+import SwiftUI
+
+struct PersonBox: View {
+    let state: InfoColorState
+    let text: String
+    let font: Font
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 4) {
+            //TODO: 아이콘 변경
+            Image(.icPeople18)
+                .resizable()
+                .frame(width: 16, height: 16)
+                .foregroundColor(iconColor)
+            
+            Text(text)
+                .font(font)
+                .foregroundColor(textColor)
+        }
+    }
+    
+    private var iconColor: Color {
+        switch state {
+        case .gray:
+            return .gray05
+        case .white:
+            return .grayWhite
+        }
+    }
+    
+    private var textColor: Color {
+        switch state {
+        case .gray:
+            return .gray06
+        case .white:
+            return .grayWhite
+        }
+    }
+    
+}
+
+
+#Preview {
+    PersonBox(state: .gray, text: "우리집!", font: .pretendard(.caption2_r_12))
+}
