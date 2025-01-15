c062f6380a476110f9deae53d793f4865a36989e
hee <heheeun13@gmail.com>
2025-01-16 05:27:08 +0900

    [Feat/#42] TitleTextBox 1차 구현

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/TitleTextBox.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/TitleTextBox.swift
@@ -5,4 +5,31 @@
 //  Created by 김희은 on 1/16/25.
 //
 
+import SwiftUI
 
+struct TitleTextBox: View {
+    let title: String
+    let subtitle: String
+    var highlightTitle: String = ""
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .pretendardFont(.head2_b_24)
+            HighlightTextView(
+                text: subtitle,
+                textColor: .black,
+                font: .body1_b_16,
+                highlightText: highlightTitle
+            )
+        }
+    }
+}
+
+#Preview {
+    TitleTextBox(
+        title: "타이틀",
+        subtitle: "서브타이틀",
+        highlightTitle: "서브"
+    )
+}
