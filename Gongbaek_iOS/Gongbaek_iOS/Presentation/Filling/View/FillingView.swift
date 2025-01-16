e4faaaa41171ce850041827bb4b91b54ea97a3c3
kms0233 <cnblue789@naver.com>
2025-01-16 22:28:13 +0900

    [Chore/#49] 폴더링

---
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/View/FillingView.swift
@@ -0,0 +1,18 @@
+//
+//  FillingView.swift
+//  Gongbaek_iOS
+//
+//  Created by 김민서 on 1/16/25.
+//
+
+import SwiftUI
+
+struct FillingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FillingView()
+}
