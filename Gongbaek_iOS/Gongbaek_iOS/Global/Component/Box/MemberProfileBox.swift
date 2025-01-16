ad24d5b24d4f964ae9fc14e759cb64dda272248f
hee <heheeun13@gmail.com>
2025-01-16 22:56:25 +0900

    [Fix/#26] memberProfileBox 수정

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/MemberProfileBox.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/MemberProfileBox.swift
@@ -8,31 +8,32 @@
 import SwiftUI
 
 struct MemberProfileBox: View {
-    var profileImage: Int
+    var profileImage: String //TODO: 추후 Int값으로 바꿔야함 - 고정값임.
     var nickname: String
     var isOwner: Bool
     
     var body: some View {
         VStack(spacing: 6) {
-            ZStack(alignment: .bottomLeading) {
-                Image("이름 넣기") // MARK: - TODO: 이미지 네이밍 수정
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .background(.gray04) // MARK: - TODO: 이미지 삽입 시 삭제
-                    .foregroundStyle(.mainOrange) // MARK: - TODO: 이미지 삽입 시 삭제
-                    .overlay(
-                        isOwner ? OwnerChip().padding(6) : nil,
-                             alignment: .bottomLeading
-                    )}
+            Image(profileImage) // MARK: - TODO: 이미지 네이밍 수정
+                .resizable()
+                .frame(width: 80, height: 80)
+                .scaledToFill()
+                .background(.gray04) // MARK: - TODO: 이미지 삽입 시 삭제
+                .foregroundStyle(.mainOrange) // MARK: - TODO: 이미지 삽입 시 삭제
+                .overlay(
+                    isOwner ? OwnerChip().padding(6) : nil,
+                    alignment: .bottomLeading
+                )
+                .padding(.horizontal, 7)
             
             Text(nickname)
                 .pretendardFont(.caption1_m_13)
                 .foregroundStyle(.gray10)
         }
-        .frame(width: 94, height: 104)
+        .background(Color.red)
     }
 }
 
 #Preview {
-    MemberProfileBox(profileImage: 1, nickname: "희은이", isOwner: true)
+    MemberProfileBox(profileImage: "sample", nickname: "희은이입니다람쥐깡깡", isOwner: true)
 }
