83bd4334395f3b2d3ec7a89998ca1d6d015013c9
kms0233 <cnblue789@naver.com>
2025-01-17 00:47:51 +0900

    [Feat/#49] FillingViewModel 구현

---
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/ViewModel/FillingViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FillingViewModel.swift
+//  Gongbaek_iOS
+//
+//  Created by 김민서 on 1/16/25.
+//
+
+import SwiftUI
+import Combine
+
+class FillingViewModel: ObservableObject {
+    @Published var isToggleOn: Bool = true
+    @Published var selectedCategory: CategoryState = .ALL {
+        didSet {
+            print("📌 선택된 카테고리 변경됨: \(selectedCategory)")
+        }
+    }
+}
