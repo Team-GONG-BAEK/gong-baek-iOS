a8c76dba9a68987a0f26647668f85c148bd8cc28
kms0233 <cnblue789@naver.com>
2025-01-17 04:02:55 +0900

    [Feat/#53] YearSelectButton 컴포넌트 구현

---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Button/YearSelectButton.swift
@@ -0,0 +1,44 @@
+//
+//  YearSelectButton.swift
+//  Gongbaek_iOS
+//
+//  Created by 김민서 on 1/17/25.
+//
+
+import SwiftUI
+
+struct YearSelectButton: View {
+    var isTapped: Bool
+    var onTap: (() -> Void)?
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("입학년도")
+                .font(.pretendard(.body2_sb_14))
+                .foregroundColor(.gray08)
+            Button(action: {}) {
+                HStack {
+                    Text("2024년")
+                        .font(.pretendard(.body1_m_16))
+                        .foregroundColor(isTapped ? .gray10 : .gray04)
+                    Spacer()
+                    Image(.icArrowBottomGray24)
+                        .foregroundColor(.gray04)
+                        .frame(width: 24, height: 24)
+                }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 12)
+            }
+            .background(.grayWhite)
+            .overlay(
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(.gray03, lineWidth: 1)
+            )
+        }
+    }
+}
+
+#Preview {
+    YearSelectButton(isTapped: true)
+        .padding(16)
+}
