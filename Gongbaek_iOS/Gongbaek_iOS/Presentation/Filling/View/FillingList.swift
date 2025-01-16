acddfbc512e3cd2a051e6d279330cd1752dd28a6
kms0233 <cnblue789@naver.com>
2025-01-17 06:51:40 +0900

    [Chore/#49] 스크롤 범위 수정

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/View/FillingList.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/View/FillingList.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct FillingList: View {
     let meetings: [Meeting] = dummyMeetings
-
+    @StateObject var viewModel = FillingViewModel()
+    
     var body: some View {
+        
         ScrollView {
-            VStack(alignment:.leading, spacing: 0) {
-                ForEach(meetings, id: \.groupTitle) { meeting in
-                    MeetingInfoCell(meeting: meeting, state: .fill)
+            VStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    CycleButton(state: .all)
+                        .disabled(true)
+                    
+                    Spacer()
+                    
+                    Text(viewModel.isToggleOn ? "겹치는 공백" : "모든 공백")
+                        .font(.pretendard(.caption2_r_12))
+                        .foregroundColor(.gray06)
+                    ToggleButton(isOn: $viewModel.isToggleOn, isDisabled: true)
+                }
+                .padding(.horizontal, 16)
+                .padding(.top, 12)
+                
+                VStack(alignment:.leading, spacing: 0) {
+                    ForEach(meetings, id: \.groupTitle) { meeting in
+                        MeetingInfoCell(meeting: meeting, state: .fill)
+                    }
                 }
             }
         }
--- Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/View/FillingView.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/View/FillingView.swift
@@ -11,25 +11,14 @@ struct FillingView: View {
     @StateObject var viewModel = FillingViewModel()
     
     var body: some View {
-        VStack(spacing: 12) {
-            VStack(spacing: 8) {
-                WeekFilterBar()
-                CategoryBar(viewModel: viewModel)
-                Rectangle()
-                    .fill(.gray01)
-                    .frame(height: 8)
-            }
-            
-            HStack(spacing: 6) {
-                CycleButton(state: .all)
-                    .disabled(true)
-                Spacer()
-                Text(viewModel.isToggleOn ? "겹치는 공백" : "모든 공백")
-                    .font(.pretendard(.caption2_r_12))
-                    .foregroundColor(.gray06)
-                ToggleButton(isOn: $viewModel.isToggleOn, isDisabled: true)
-            }
-            .padding(.horizontal, 16)
+        VStack(spacing: 0) {
+            WeekFilterBar()
+                .padding(.bottom, 8)
+            CategoryBar(viewModel: viewModel)
+                .padding(.bottom, 8)
+            Rectangle()
+                .fill(.gray01)
+                .frame(height: 8)
             FillingList()
             Spacer()
         }
