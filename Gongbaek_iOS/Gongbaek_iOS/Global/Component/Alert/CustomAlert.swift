cd3365a7a935f22a6e98c9ff8a835d5021d658c0
hee <heheeun13@gmail.com>
2025-01-16 23:39:22 +0900

    [Fix/#32] Alert 조건에 따른 logic 수정

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Alert/CustomAlert.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Alert/CustomAlert.swift
@@ -7,77 +7,43 @@
 
 import SwiftUI
 
-// MARK: - TODO: enum 분리
-
-enum AlertType {
-    case basic
-    case subtitle
-    case grayButton
-    case subtitleGrayButton
-}
-
-extension AlertType {
-    var isSubtitle: Bool {
-        switch self {
-        case .subtitle, .subtitleGrayButton:
-            return true
-        default:
-            return false
-        }
-    }
-    
-    var isGrayButton: Bool {
-        switch self {
-        case .grayButton, .subtitleGrayButton:
-            return true
-        default:
-            return false
-        }
-    }
-}
-
 // MARK: - TODO: Model 분리
 
-struct AlertData {
-    var alertImage: String = "person.fill"
-    var titleText: String = "title"
-    var subtitleText: String = "subtitle"
-    var grayButtonText: String = "close"
-    var orangeButtonText: String = "next"
+struct CustomedAlert: View {
+    let alertImage: String
+    let titleText: String
+    var subtitleText: String? = nil
+    var grayButtonText: String? = nil
+    let orangeButtonText: String
     var onTapGrayButton: (() -> Void)?
     var onTapOrangeButton: (() -> Void)?
-}
-
-struct CustomedAlert: View {
-    let alertType: AlertType
-    var alertData: AlertData
     
     var body: some View {
         ZStack {
             BackgroundBlack()
             
             VStack (alignment: .center, spacing: 0) {
-                Image(alertData.alertImage) // MARK: - TODO: 이미지 수정
+                Image(alertImage) // MARK: - TODO: 이미지 수정
                     .resizable()
                     .background(.gray02) // MARK: - TODO: 이미지 수정 시 삭제
                     .frame(width: 178, height: 178)
                     .padding(.horizontal, 20)
                 
                 VStack(spacing: 0) {
-                    TitleTextBox(text: alertData.titleText)
+                    TitleTextBox(text: titleText)
                     
-                    alertType.isSubtitle ? SubtitleTextBox(text: alertData.subtitleText) : nil
+                    subtitleText != nil ? SubtitleTextBox(text: subtitleText ?? "") : nil
                 }
                 .padding(20)
-                .padding(.top, alertType.isSubtitle ? 8 : 0) // subtitle 유무에 따라, VStack에 Padding 추가 삽입
+                .padding(.top, subtitleText != nil ? 8 : 0)
                 
                 HStack(alignment: .center, spacing: 10) {
-                    alertType.isGrayButton ? GrayButton(buttonText: alertData.grayButtonText, buttonAction: alertData.onTapGrayButton) : nil
+                    grayButtonText != nil ? GrayButton(buttonText: grayButtonText ?? "", buttonAction: onTapGrayButton) : nil
                     
                     Button(action: {
-                        alertData.onTapOrangeButton?()
+                        onTapOrangeButton?()
                     }) {
-                        Text(alertData.orangeButtonText)
+                        Text(orangeButtonText)
                             .pretendardFont(.title2_sb_18)
                             .padding(.vertical, 16)
                             .frame(maxWidth: .infinity)
@@ -165,15 +131,12 @@ struct GrayButton: View {
 
 #Preview {
     CustomedAlert(
-        alertType: AlertType.subtitleGrayButton,
-        alertData: AlertData(
-            alertImage: "sample",
-            titleText: "모임등록이완료되었다면믿으시겠습니까욥",
-            subtitleText: "아니요 모르겠고 내 뷰나 책입져 이자식아!!! 나 집에 갈래!!! 으악으악\n으악아악악!!!!",
-            grayButtonText: "닫기",
-            orangeButtonText: "다음으로",
-            onTapGrayButton: nil,
-            onTapOrangeButton: nil
-        )
+        alertImage: "sample",
+        titleText: "모임등록이완료되었다면믿으시겠습니까욥",
+        subtitleText: "아니요 모르겠고 내 뷰나 책입져 이자식아!!! 나 집에 갈래!!! 으악으악\n으악아악악!!!!",
+        grayButtonText: "닫기",
+        orangeButtonText: "다음으로",
+        onTapGrayButton: nil,
+        onTapOrangeButton: nil
     )
 }
