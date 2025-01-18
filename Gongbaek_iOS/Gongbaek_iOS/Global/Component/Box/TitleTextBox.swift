115f7bffe3c53b171cd5fcdc7020bd1a8aef254c
hee <heheeun13@gmail.com>
2025-01-18 21:25:10 +0900

    [Fix/#66] subtitle 없는 경우 생성

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/TitleTextBox.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/TitleTextBox.swift
@@ -9,20 +9,21 @@ import SwiftUI
 
 struct TitleTextBox: View {
     let title: String
-    let subtitle: String
+    var subtitle: String? = nil
     var highlightSubtitleText: String = ""
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             Text(title)
                 .pretendardFont(.head2_b_24)
-            
-            HighlightTextView(
-                text: subtitle,
-                textColor: .gray07,
-                font: .body1_m_16,
-                highlightString: highlightSubtitleText
-            )
+            if let subtitle = subtitle {
+                HighlightTextView(
+                    text: subtitle,
+                    textColor: .gray07,
+                    font: .body1_m_16,
+                    highlightString: highlightSubtitleText
+                )
+            }
         }
     }
 }
@@ -31,6 +32,11 @@ struct TitleTextBox: View {
     TitleTextBox(
         title: "공갱갱히히 공강시간에 원하는 모임 만들기",
         subtitle: "공강이라는 공백, 어떻게 채우고 있나요? 이제 원하는 모임으로 특별하게 채워보세요. 근데 나는 희은이랑 공강강갱갱할랭 ㅋㅋ",
+        highlightSubtitleText: "ㅋㅋ"
+    )
+    TitleTextBox(
+        title: "공갱갱히히 공강시간에 원하는 모임 만들기",
+        subtitle: nil,
         highlightSubtitleText: "ㅋㅋ"
     )
 }
