5a4bfa96c9d08da74448a7df229c03bab3ae4220
kms0233 <cnblue789@naver.com>
2025-01-18 23:33:20 +0900

    [Feat/#65] SelectedTimeBox 구현

---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/SelectedTimeBox.swift
@@ -0,0 +1,30 @@
+//
+//  SelectedTimeBox.swift
+//  Gongbaek_iOS
+//
+//  Created by 김민서 on 1/18/25.
+//
+
+import SwiftUI
+
+struct SelectedTimeBox: View {
+    var time: String
+    
+    var body: some View {
+        Text(time)
+            .font(.pretendard(.title2_b_18))
+            .foregroundColor(.gray09)
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 10)
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.gray02, lineWidth: 1)
+            }
+        
+        
+    }
+}
+
+#Preview {
+    SelectedTimeBox(time: "00:00")
+}
