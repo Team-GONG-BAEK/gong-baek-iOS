b7551798b4e834eca78c66c00ff21c3a0484e480
kms0233 <cnblue789@naver.com>
2025-01-19 08:58:07 +0900

    [Feat/#65] AddMeetingView View로 분리

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/AddMeetingView.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/AddMeetingView.swift
@@ -1,5 +1,5 @@
 //
-//  AddMeetingView.swift
+//  CycleSelect.swift
 //  Gongbaek_iOS
 //
 //  Created by 김민서 on 1/18/25.
@@ -8,43 +8,49 @@
 import SwiftUI
 
 struct AddMeetingView: View {
-    @StateObject var viewModel = AddMeetingViewModel()
-    
-    
+    @StateObject var viewModel: AddMeetingViewModel
+
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 0) {
             ProgressBar(currentIndex: $viewModel.currentIndex)
                 .padding(.bottom, 40)
             
-            switch viewModel.currentIndex {
-            case 0:
-                CycleSelect(viewModel: viewModel)
-            case 1:
-                if viewModel.selectedCycle == .once {
-                    CalendarSelect(viewModel: viewModel)
-                } else if viewModel.selectedCycle == .weekly {
-                    WeekDaySelect(viewModel: viewModel)
+            VStack(alignment: .leading, spacing: 0) {
+                TitleTextBox(title: "활동주기를 선택해주세요.", subtitle: nil)
+                    .padding(.bottom, 20)
+                
+                HStack(spacing: 8) {
+                    SmallButton(text: "한번만 볼래요", isTapped: viewModel.selectedCycle == .once) {
+                        viewModel.selectedCycle = .once
+                        viewModel.isNextEnabled = true
+                    }
+                    SmallButton(text: "매주 볼래요", isTapped: viewModel.selectedCycle == .weekly) {
+                        viewModel.selectedCycle = .weekly
+                        viewModel.isNextEnabled = true
+                    }
                 }
-            case 2:
-                TimeSelect(viewModel: viewModel)
-            case 3:
-                CategorySelect(viewModel: viewModel)
-            case 4:
-                CoverImageSelect(viewModel: viewModel)
-            case 5:
-                LocationInput(showError: false, isFocused: false)
-            case 6:
-                IntroduceInput(showError: false, isFocused: false)
-            default:
-                CheckInputInfo()
+                .padding(.bottom, 48)
+                
+                HStack {
+                    Image(.icMark16)
+                        .foregroundColor(.mainOrange)
+                        .frame(width: 16, height: 16)
+                    Text("매주 볼래요의 경우 모임은 한 학기동안 유효합니다.")
+                        .pretendardFont(.body2_sb_14)
+                        .foregroundColor(.mainOrange)
+                }
+                .padding(.bottom, 10)
+                
+                Text("1학기는 6월 30일 0시까지, 2학기는 12월 31일 0시까지가 기준입니다.\n모임 개설자는 원한다면 중간에 언제든 모임을 삭제할 수 있습니다.")
+                    .pretendardFont(.caption2_r_12)
+                    .foregroundColor(.gray08)
             }
+            .padding(.horizontal, 16)
             
             Spacer()
-            
             BasicButton(text: "다음", isActivated: viewModel.isNextEnabled) {
                 viewModel.goToNextPage()
             }
-            
             .padding(.vertical, 20)
             .padding(.horizontal, 16)
         }
@@ -52,7 +58,6 @@ struct AddMeetingView: View {
 }
 
 #Preview {
-    @Previewable @State var viewModel = AddMeetingViewModel()
-
-    CoverImageSelect(viewModel: viewModel)
+    let viewModel = AddMeetingViewModel()
+    AddMeetingView(viewModel: viewModel)
 }
