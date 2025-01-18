e2f423253463b7a88ac77b6051543350c1035bcd
kms0233 <cnblue789@naver.com>
2025-01-18 17:07:40 +0900

    [Feat/#61] MyFillingView UI 구현

---
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/MyFilling/View/MyFillingView.swift
@@ -0,0 +1,18 @@
+//
+//  MyFillingView.swift
+//  Gongbaek_iOS
+//
+//  Created by 김민서 on 1/18/25.
+//
+
+import SwiftUI
+
+struct MyFillingView: View {
+    var body: some View {
+        CustomSegmentControlBar(segmentState: .myfill)
+    }
+}
+
+#Preview {
+    MyFillingView()
+}
