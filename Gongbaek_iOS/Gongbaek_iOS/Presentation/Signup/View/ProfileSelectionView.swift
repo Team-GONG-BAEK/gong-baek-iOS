88ca3dcfb92029b7357fa7ef583836f4fece16c4
김나연 <nanalovesbap@gmail.com>
2025-01-19 01:15:22 +0900

    [Feat/#63] 회원가입 로직 화면 내비게이션 스택 정의 (구 네이밍: 온보딩)

---
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/Signup/View/ProfileSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileSelectionView.swift
+//  Gongbaek_iOS
+//
+//  Created by 김나연 on 1/19/25.
+//
+
+import SwiftUI
+
+struct ProfileSelectionView: View {
+    @EnvironmentObject var navigationManager: NavigationManager
+    
+    var body: some View {
+        Text("프로필 선택")
+    }
+}
+
+#Preview {
+    HomeView()
+}
