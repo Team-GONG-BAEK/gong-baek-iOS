366a8574143212e0cc8a00e9bd06c7ba382ed270
hee <heheeun13@gmail.com>
2025-01-19 11:20:35 +0900

    [Fix/#70] 생성된 Model에 따라 컴포넌트 네이밍 수정

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/MemberProfileBox.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/MemberProfileBox.swift
@@ -8,19 +8,17 @@
 import SwiftUI
 
 struct MemberProfileBox: View {
-    var profileImage: String //TODO: 추후 Int값으로 바꿔야함 - 고정값임.
-    var nickname: String
-    var isOwner: Bool
+    let meetingRoomMember: MeetingRoomMember
     
     var body: some View {
         VStack(spacing: 6) {
-            Image(profileImage) //TODO: 이미지 네이밍 수정
+            Image("\(meetingRoomMember.profileImage)") //TODO: 이미지 네이밍 수정
                 .resizable()
                 .scaledToFill()
                 .frame(width: 80, height: 80)
                 .clipShape(RoundedRectangle(cornerRadius: 2))
                 .overlay(
-                    isOwner ? OwnerChip(ownerChipType: .gray09).padding(6) : nil,
+                    meetingRoomMember.isHost ? OwnerChip(ownerChipType: .gray09).padding(6) : nil,
                     alignment: .bottomLeading
                 )
                 .overlay(
@@ -29,14 +27,10 @@ struct MemberProfileBox: View {
                 )
                 .padding(.horizontal, 7)
  
-            Text(nickname)
+            Text(meetingRoomMember.nickname)
                 .pretendardFont(.caption1_m_13)
                 .foregroundStyle(.gray10)
         }
         .background(.grayWhite)
     }
-}
-
-#Preview {
-    MemberProfileBox(profileImage: "sample", nickname: "희은이입니다람쥐깡깡", isOwner: true)
 }
--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/List/CommentCell.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/List/CommentCell.swift
@@ -8,39 +8,42 @@
 import SwiftUI
 
 struct Comment {
-    var id = UUID()
-    var isOwner: Bool
+    var commentId = UUID()
+    var isGroupHost: Bool
     var isWriter: Bool
-    var nickmane: String
-    var content: String
-    var date: String
+    var nickname: String
+    var body: String
+    var createdAt: String
 }
 
 struct CommentCell: View {
     let comment: Comment
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            HStack(alignment: .center, spacing: 6) {
-                Text(comment.nickmane)
-                    .pretendardFont(.body1_sb_16)
-                comment.isOwner ? OwnerChip() : nil
-                Spacer()
-                comment.isWriter ? DeleteButton() : nil
+        ZStack(alignment: .bottom) {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(alignment: .center, spacing: 6) {
+                    Text(comment.nickname)
+                        .pretendardFont(.body1_sb_16)
+                    comment.isGroupHost ? OwnerChip() : nil
+                    Spacer()
+                    comment.isWriter ? DeleteButton() : nil
+                }
+                .foregroundColor(.grayBlack)
+                .padding(.bottom, 8)
+                
+                Text(comment.body)
+                    .pretendardFont(.body2_r_14)
+                    .foregroundColor(.gray08)
+                    .padding(.bottom, 4)
+                
+                Text(comment.createdAt)
+                    .pretendardFont(.caption2_r_12)
+                    .foregroundColor(.gray05)
             }
-            .foregroundColor(.grayBlack)
-            .padding(.bottom, 8)
-
-            Text(comment.content)
-                .pretendardFont(.body2_r_14)
-                .foregroundColor(.gray08)
-                .padding(.bottom, 4)
-            
-            Text(comment.date)
-                .pretendardFont(.caption2_r_12)
-                .foregroundColor(.gray05)
+            .padding(.vertical, 14)
+            Color.gray02.frame(height: 1)
         }
-        .padding(.vertical, 14)
     }
 }
 
@@ -55,8 +58,4 @@ struct DeleteButton: View {
                 .foregroundStyle(.gray05)
         }
     }
-}
-
-#Preview {
-    CommentCell(comment: Comment(isOwner: true, isWriter: true, nickmane: "희은이", content: "난 로이임탄이랑 하기 시러시러 !", date: "12/27/12:05"))
 }
