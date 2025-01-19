3a538bf314a42a2a7c7e1eaa6761c6c977818457
kms0233 <cnblue789@naver.com>
2025-01-19 09:02:52 +0900

    [Feat/#65] WeekDaySelect View로 분리

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/WeekDaySelect.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/WeekDaySelect.swift
@@ -8,26 +8,38 @@
 import SwiftUI
 
 struct WeekDaySelect: View {
-    @ObservedObject var viewModel: AddMeetingViewModel
+    @StateObject var viewModel: AddMeetingViewModel
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            TitleTextBox(title: "활동주기를 선택해주세요.", subtitle: nil)
-                .padding(.bottom, 60)
+            ProgressBar(currentIndex: $viewModel.currentIndex)
+                .padding(.bottom, 40)
             
-            VStack(spacing: 16) {
-                ForEach(WeekDay.allCases, id: \.self) { day in
-                    SmallButton(
-                        text: day.koreanName,
-                        isTapped: viewModel.selectedWeekDay == day
-                    ){
-                        viewModel.selectedWeekDay = day
-                        viewModel.isNextEnabled = true
+            VStack(alignment: .leading, spacing: 0) {
+                TitleTextBox(title: "활동주기를 선택해주세요.", subtitle: nil)
+                    .padding(.bottom, 60)
+                
+                VStack(spacing: 16) {
+                    ForEach(WeekDay.allCases, id: \.self) { day in
+                        SmallButton(
+                            text: day.koreanName,
+                            isTapped: viewModel.selectedWeekDay == day
+                        ){
+                            viewModel.selectedWeekDay = day
+                            viewModel.isNextEnabled = true
+                        }
                     }
                 }
             }
+            .padding(.horizontal, 16)
+            
+            Spacer()
+            BasicButton(text: "다음", isActivated: viewModel.isNextEnabled) {
+                viewModel.goToNextPage()
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 16)
         }
-        .padding(.horizontal, 16)
     }
 }
 
