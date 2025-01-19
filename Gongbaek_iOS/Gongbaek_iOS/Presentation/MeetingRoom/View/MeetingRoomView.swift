95ba8769f054e5a3d0d5fa3d3678b4d8f5e0d59e
hee <heheeun13@gmail.com>
2025-01-19 16:21:24 +0900

    [Fix/#70] ScrollView 영역 다시 설정

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/MeetingRoom/View/MeetingRoomView.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/MeetingRoom/View/MeetingRoomView.swift
@@ -11,98 +11,98 @@ struct MeetingRoomView: View {
     @State var meetingDetailData: MeetingDetailData
     @State var memberData: MeetingRoomMemberData
     @State var commentData: MeetingRoomCommentData
-    let items = ["HEEEUN", "MS", "NY"]
     
     var body: some View {
-        VStack(spacing: 0) {
-            VStack(alignment: .leading, spacing: 0) {
-                HStack {
-                    MeetingChip(state: .recruiting(.closed))
-                    MeetingChip(state: .category(.DINING))
-                    MeetingChip(state: .weekly(true))
-                }
-                .padding(.top, 18)
-                .padding(.bottom, 6)
-                
-                Text(meetingDetailData.groupTitle)
-                    .pretendardFont(.title1_b_20)
-                    .foregroundColor(.grayWhite)
-                    .lineLimit(nil)
-                    .padding(.bottom, 12)
-                
-                TimeBox(state: .white, text: String(meetingDetailData.startTime), font: .pretendard(.caption2_r_12))
-                    .padding(.bottom, 2)
-                
-                LocationBox(state: .white, text: meetingDetailData.location, font: .pretendard(.caption2_r_12))
-            }
-            .padding(.horizontal, 16)
-            .padding(.bottom, 16)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .background(
-                Image("sample")
-                    .resizable()
-                    .scaledToFill()
-            )
-            
-            VStack(alignment: .leading, spacing: 0) {
-                HStack {
-                    Image(.icPeople18)
-                        .resizable()
-                        .frame(width: 18, height: 18)
-                        .foregroundStyle(.gray06)
-                    Text("멤버 (\(meetingDetailData.currentPeopleCount)/\(meetingDetailData.maxPeopleCount)명)")
-                        .pretendardFont(.title2_sb_18)
-                        .foregroundStyle(.gray10)
-                }
-                .padding(.top, 16)
-                .padding(.bottom, 12)
-                .padding(.horizontal, 16)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(.grayWhite)
-            }
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 6) {
-                    ForEach(memberData.members.indices, id: \.self) { index in
-                        MemberProfileBox(meetingRoomMember: dummyMeetingRoomMemberData.members[index])
-                    }
-                }
-                .padding(.horizontal, 9)
-                .padding(.bottom, 16)
-            }
-            .background(.grayWhite)
-            Color.gray02.frame(height: 8)
-            
-            ZStack(alignment: .bottom) {
+        ZStack(alignment: .bottom) {
+            ScrollView {
                 VStack(spacing: 0) {
-                    HStack(alignment: .center) {
-                        Text("댓글 \(commentData.commentCount)개")
-                            .pretendardFont(.body1_sb_16)
-                            .frame(alignment: .leading)
-                            .padding(.vertical, 16)
-                        Spacer()
-                        Button(action: {}) {
-                            Image(.icRefresh32)
-                                .foregroundStyle(.gray05)
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack {
+                            MeetingChip(state: .recruiting(.closed))
+                            MeetingChip(state: .category(.DINING))
+                            MeetingChip(state: .weekly(true))
                         }
+                        .padding(.top, 18)
+                        .padding(.bottom, 6)
+                        
+                        Text(meetingDetailData.groupTitle)
+                            .pretendardFont(.title1_b_20)
+                            .foregroundColor(.grayWhite)
+                            .lineLimit(nil)
+                            .padding(.bottom, 12)
+                        
+                        TimeBox(state: .white, text: String(meetingDetailData.startTime), font: .pretendard(.caption2_r_12))
+                            .padding(.bottom, 2)
+                        
+                        LocationBox(state: .white, text: meetingDetailData.location, font: .pretendard(.caption2_r_12))
                     }
-                    .padding(.leading, 16)
-                    .padding(.trailing, 8)
-                    .frame(maxWidth: .infinity)
-                    .background(.grayWhite)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 16)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(
+                        Image("sample")
+                            .resizable()
+                            .scaledToFill()
+                    )
                     
-                    ScrollView {
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack {
+                            Image(.icPeople18)
+                                .resizable()
+                                .frame(width: 18, height: 18)
+                                .foregroundStyle(.gray06)
+                            Text("멤버 (\(meetingDetailData.currentPeopleCount)/\(meetingDetailData.maxPeopleCount)명)")
+                                .pretendardFont(.title2_sb_18)
+                                .foregroundStyle(.gray10)
+                        }
+                        .padding(.top, 16)
+                        .padding(.bottom, 12)
+                        .padding(.horizontal, 16)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(.grayWhite)
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 6) {
+                            ForEach(memberData.members.indices, id: \.self) { index in
+                                MemberProfileBox(meetingRoomMember: dummyMeetingRoomMemberData.members[index])
+                            }
+                        }
+                        .padding(.horizontal, 9)
+                        .padding(.bottom, 16)
+                    }
+                    .background(.grayWhite)
+                    Color.gray02.frame(height: 8)
+                    
+                    VStack(spacing: 0) {
+                        HStack(alignment: .center) {
+                            Text("댓글 \(commentData.commentCount)개")
+                                .pretendardFont(.body1_sb_16)
+                                .frame(alignment: .leading)
+                                .padding(.vertical, 16)
+                            Spacer()
+                            Button(action: {}) {
+                                Image(.icRefresh32)
+                                    .foregroundStyle(.gray05)
+                            }
+                        }
+                        .padding(.leading, 16)
+                        .padding(.trailing, 8)
+                        .frame(maxWidth: .infinity)
+                        .background(.grayWhite)
+                        
                         VStack(alignment:.leading, spacing: 0) {
                             ForEach (commentData.comments.indices, id: \.self) { index in
                                 CommentCell(comment: commentData.comments[index])
                             }
                         }
+                        .padding(.bottom, 70)
                         .padding(.horizontal, 16)
                         .background(.grayWhite)
                     }
-                    CommentTextField()
                 }
             }
+            CommentTextField()
         }
     }
 }
