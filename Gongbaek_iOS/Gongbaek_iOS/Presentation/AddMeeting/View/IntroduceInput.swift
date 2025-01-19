a4729d1b8b48cae153d1d3a6775458351ff3dae8
kms0233 <cnblue789@naver.com>
2025-01-19 09:05:47 +0900

    [Feat/#65] IntroduceInput View로 분리

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/IntroduceInput.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/IntroduceInput.swift
@@ -8,27 +8,47 @@
 import SwiftUI
 
 struct IntroduceInput: View {
-    @State private var location: String = ""
+    @StateObject var viewModel: AddMeetingViewModel
+
     @State var showError: Bool
     @State var isFocused: Bool
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            TitleTextBox(title: "소개글을 작성해주세요.", subtitle: nil)
+        VStack(alignment: .leading, spacing: 0){
+            ProgressBar(currentIndex: $viewModel.currentIndex)
+                .padding(.bottom, 40)
+            
+            VStack(alignment: .leading, spacing: 0) {
+                TitleTextBox(title: "소개글을 작성해주세요.", subtitle: nil)
+                    .padding(.bottom, 28)
+                
+                CustomTextField(
+                    text: $viewModel.title,
+                    showError: $showError,
+                    state: .title
+                )
                 .padding(.bottom, 28)
+                .onChange(of: viewModel.title) { _ in
+                    viewModel.updateNextButtonState()
+                }
+                
+                CustomTextEditor(text: $viewModel.introduction)
+                    .onChange(of: viewModel.introduction) { _ in
+                        viewModel.updateNextButtonState()
+                    }
+            }
+            .padding(.horizontal, 16)
             
-            CustomTextField(
-                text: $location,
-                showError: $showError,
-                state: .title
-            )
-            
-            CustomTextEditor()
+            Spacer()
+            BasicButton(text: "다음", isActivated: viewModel.isNextEnabled) {
+                viewModel.goToNextPage()
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 16)
         }
-        .padding(.horizontal)
     }
 }
 
-#Preview {
-    IntroduceInput(showError: false, isFocused: false)
-}
+//#Preview {
+//    IntroduceInput(showError: false, isFocused: false)
+//}
