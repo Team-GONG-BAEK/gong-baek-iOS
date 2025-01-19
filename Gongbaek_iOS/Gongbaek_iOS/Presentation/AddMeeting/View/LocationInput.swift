117bc61cc4f920d3ad4432b6f07da208d84819ec
kms0233 <cnblue789@naver.com>
2025-01-19 09:05:00 +0900

    [Feat/#65] LocationInput View로 분리

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/LocationInput.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/AddMeeting/View/LocationInput.swift
@@ -8,35 +8,59 @@
 import SwiftUI
 
 struct LocationInput: View {
+    @StateObject var viewModel: AddMeetingViewModel
+
     @State private var location: String = ""
     @State var showError: Bool
     @State var isFocused: Bool
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            TitleTextBox(title: "활동주기를 선택해주세요.", subtitle: nil)
-                .padding(.bottom, 20)
+        VStack(alignment: .leading, spacing: 0){
+            ProgressBar(currentIndex: $viewModel.currentIndex)
+                .padding(.bottom, 40)
             
-            CustomTextField(
-                text: $location,
-                showError: $showError,
-                state: .location
-            )
-            
-            TitleTextBox(title: "활동주기를 선택해주세요.", subtitle: "본인 포함 최소 2명부터 최대 10명까지 모집 가능해요.")
-                .padding(.bottom, 20)
-            
-            HStack(spacing: 10) {
-                CounterButton(state: .decrease)
-                PersonCountBox()
-                CounterButton(state: .increase)
+            VStack(alignment: .leading, spacing: 0) {
+                TitleTextBox(title: "활동주기를 선택해주세요.", subtitle: nil)
+                    .padding(.bottom, 20)
+                
+                CustomTextField(
+                    text: $viewModel.location,
+                    showError: $showError,
+                    state: .location
+                )
+                .padding(.bottom, 40)
+                .onChange(of: viewModel.location) { newValue in
+                    viewModel.updateNextButtonState()
+                }
+                
+                TitleTextBox(title: "인원을 선택해주세요.", subtitle: "본인 포함 최소 2명부터 최대 10명까지 모집 가능해요.")
+                    .padding(.bottom, 20)
+                
+                HStack(spacing: 10) {
+                    CounterButton(state: .decrease, onTap: {
+                        viewModel.decreasePeopleCount()
+                    })
+                    PersonCountBox(num: viewModel.maxPeopleCount)
+                    CounterButton(state: .increase, onTap: {
+                        viewModel.increasePeopleCount()
+                    })
+                }
+                
             }
+            .padding(.horizontal, 16)
             
+            Spacer()
+            BasicButton(text: "다음", isActivated: viewModel.isNextEnabled) {
+                viewModel.goToNextPage()
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 16)
         }
-        .padding(.horizontal, 16)
+        
     }
 }
 
 #Preview {
-    LocationInput(showError: false, isFocused: false)
+    let viewModel = AddMeetingViewModel()
+    LocationInput(viewModel: viewModel, showError: false, isFocused: false)
 }
