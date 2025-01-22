8983368d1f9ee1348c14b76e9336cce3404b05a2
hee <heheeun13@gmail.com>
2025-01-23 04:01:12 +0900

    [Feat/#99] MeetingDetails GET logic 추가

--- Gongbaek_iOS/Gongbaek_iOS/Network/Service/TargetType/MeetingRoomTargetType.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Network/Service/TargetType/MeetingRoomTargetType.swift
@@ -9,12 +9,15 @@ import Moya
 
 enum MeetingRoomTargetType {
     case getMembers(groupId: Int, groupType: String)
+    case getMeetingDetails(groupId: Int, groupType: String)
 }
 
 extension MeetingRoomTargetType: BaseTargetType {
     var headers: Parameters? {
         switch self {
         case .getMembers:
+            return APIConstants.hasTokenHeader
+        case .getMeetingDetails:
             return APIConstants.hasTokenHeader
         }
     }
@@ -23,6 +26,8 @@ extension MeetingRoomTargetType: BaseTargetType {
         switch self {
         case .getMembers:
             return "/api/v1/fill/members"
+        case .getMeetingDetails:
+            return "/api/v1/fill/info"
         }
     }
     
@@ -30,12 +35,22 @@ extension MeetingRoomTargetType: BaseTargetType {
         switch self {
         case .getMembers:
             return .get
+        case .getMeetingDetails:
+            return .get
         }
     }
     
     var task: Moya.Task {
         switch self {
         case .getMembers(let groupId, let groupType):
+            return .requestParameters(
+                parameters: [
+                    "groupId": groupId,
+                    "groupType": groupType
+                ],
+                encoding: URLEncoding.queryString
+            )
+        case .getMeetingDetails(let groupId, let groupType):
             return .requestParameters(
                 parameters: [
                     "groupId": groupId,
