f301019c1b965d968412a7864da1e776c2b3e54a
김나연 <nanalovesbap@gmail.com>
2025-01-25 14:13:39 +0900

    [Feat/#142] 자동로그인 구현 전 회원가입 건너뛰기 투명 비밀 버튼 구현

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/Onboarding/OnboardingNavigation.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/Onboarding/OnboardingNavigation.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct OnboardingNavigation: View {
+    @EnvironmentObject var navigationManager: NavigationManager
     @Binding var currentPage: Int
     let pageCount: Int
     
@@ -25,6 +26,17 @@ struct OnboardingNavigation: View {
                 Spacer().frame(width: 48, height: 48)
             }
             Spacer()
+            
+            /// (자동로그인 구현 전) 회원가입 건너뛰기 비밀 버튼
+            Button {
+                withAnimation {
+                    navigationManager.rootView = .tabBar
+                }
+            } label: {
+                Rectangle()
+                    .fill(.white)
+                    .frame(width: 48, height: 48)
+            }
         }
         .padding(.bottom, 54)
     }
