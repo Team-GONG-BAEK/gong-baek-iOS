9fed9f3797403f77c2a1c09ef723b1605d8e4e14
hee <heheeun13@gmail.com>
2025-03-28 03:10:05 +0900

    [Feat/#6] Safari 외부 링크로 버튼 연결

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/MyFilling/MypageSettingView.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/MyFilling/MypageSettingView.swift
@@ -11,9 +11,17 @@ struct MypageSettingView: View {
     var body: some View {
         VStack(spacing: 0) {
             SettingHeaderCell(title: "이용 안내")
-            SettingBasicCell(title: "공지사항", bodyText: nil)
-            SettingBasicCell(title: "개인정보 처리방침", bodyText: nil)
-            SettingBasicCell(title: "서비스 이용 약관", bodyText: nil)
+
+            Link(destination: URL(string: "https://booming-jasmine-4c1.notion.site/1b34c7511f4280a9805df2e84a4ae342")!) {
+                            SettingBasicCell(title: "공지사항", bodyText: nil)
+                        }
+            Link(destination: URL(string: "https://booming-jasmine-4c1.notion.site/1b34c7511f428057ad33e49f07f6125a")!) {
+                SettingBasicCell(title: "개인정보 처리방침", bodyText: nil)
+            }
+            Link(destination: URL(string: "https://booming-jasmine-4c1.notion.site/1b34c7511f4280fc8dabdb19fff63765")!) {
+                SettingBasicCell(title: "서비스 이용 약관", bodyText: nil)
+            }
+            
             SettingBasicCell(title: "버전 정보", bodyText: "1.1.1")
             
             Color(.gray01)
