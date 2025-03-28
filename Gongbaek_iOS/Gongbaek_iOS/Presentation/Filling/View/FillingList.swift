3741f60a9cb80e91a8dc1274c2085a11abd3f51d
kms0233 <cnblue789@naver.com>
2025-03-28 14:09:05 +0900

    [Fix/#11] 필터링 바 수정

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/View/FillingList.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/Filling/View/FillingList.swift
@@ -16,23 +16,8 @@ struct FillingList: View {
         
         ScrollView {
             VStack(spacing: 0) {
-                HStack(spacing: 6) {
-                    CycleButton(state: .all)
-                        .disabled(true)
-                    
-                    Spacer()
-                    
-                    Text(viewModel.isToggleOn ? "겹치는 공백" : "모든 공백")
-                        .font(.pretendard(.caption2_r_12))
-                        .foregroundColor(.gray06)
-                    ToggleButton(isOn: $viewModel.isToggleOn, isDisabled: true)
-                }
-                .padding(.horizontal, 16)
-                .padding(.top, 12)
-                
                 if meetings.isEmpty {
                     FillingEmptyView()
-                        .padding(.top, 39)
                 } else {
                     VStack(alignment:.leading, spacing: 0) {
                         ForEach(meetings, id: \..groupTitle) { meeting in
