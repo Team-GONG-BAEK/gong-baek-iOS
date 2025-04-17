1fff0788e168b092f2aee845e5dacfad343dc9b4
hee <heheeun13@gmail.com>
2025-04-17 15:46:06 +0900

    [Chore/#16] 코드 extension으로 리팩토링

--- Gongbaek_iOS/Gongbaek_iOS/Presentation/MeetingDetail/View/MeetingDetailView.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Presentation/MeetingDetail/View/MeetingDetailView.swift
@@ -31,80 +31,119 @@ struct MeetingDetailView: View {
                 viewModel.fetchAllData(groupId: groupId, groupType: groupType)
             }
             
-            if viewModel.showAlert {
+            
+            applyAlert()
+            cancelAlert()
+            deleteAlert()
+            defaultFullErrorView()
+            defaultErrorAlert()
+        }
+    }
+    
+    func divider() -> some View {
+        Color.gray02.frame(height: 8)
+    }
+}
+
+extension MeetingDetailView {
+    @ViewBuilder
+    func applyAlert() -> some View {
+        if viewModel.showRecruitedAlert {
+            CustomedAlert(
+                alertImage: "img_fail" ,
+                titleText: "인원이 마감되어 신청이 불가능해요!",
+                subtitleText: "아쉽지만, 다른 모임을 찾아보세요.",
+                orangeButtonText: "확인",
+                onTapOrangeButton: {
+                    viewModel.showRecruitedAlert = false
+                }
+            )
+        }
+        else {
+            if viewModel.showApplySuccessAlert {
                 CustomedAlert(
                     alertImage: viewModel.isSuccessGetData ? "img_success" : "img_fail" ,
-                    titleText: viewModel.isSuccessGetData ? "신청이 완료됐어요!" : "인원이 마감되어 신청이 불가능해요!",
-                    subtitleText: viewModel.isSuccessGetData ? "생성자에 의해 모임이 삭제될 수도 있어요." : "아쉽지만, 다른 모임을 찾아보세요.",
+                    titleText: viewModel.isSuccessGetData ? "신청이 완료됐어요!" : "일시적인 오류가 발생했습니다.",
+                    subtitleText: viewModel.isSuccessGetData ? "생성자에 의해 모임이 삭제될 수도 있어요." : "잠시 후 다시 시도해주세요.",
                     grayButtonText: viewModel.isSuccessGetData ? "닫기" : nil,
                     orangeButtonText: viewModel.isSuccessGetData ? "스페이스 바로가기" : "확인",
                     onTapGrayButton: {
-                        viewModel.showAlert = false
+                        viewModel.showApplySuccessAlert = false
                     },
                     onTapOrangeButton: {
-                        viewModel.showAlert = false
+                        viewModel.showApplySuccessAlert = false
                         if viewModel.isSuccessGetData {
-                            navigationManager.push(
-                                view: MeetingRoomDestination.meetingRoom(
-                                    groupId: groupId,
-                                    groupType: groupType
-                                ))
+                            navigationManager
+                                .push(
+                                    view: MeetingRoomDestination
+                                        .meetingRoom(
+                                            groupId: groupId,
+                                            groupType: groupType
+                                        )
+                                )
                         }
-                    }
-                )
-            }
-            
-            if viewModel.showPatchAlert {
-                CustomedAlert(
-                    alertImage: viewModel.isSuccessGetData ? "img_fail" : "img_fail" ,
-                    titleText: viewModel.isSuccessGetData ? "모임 신청이 취소되었어요!" : "알 수 없는 이유로 신청 취소가 거부되었습니다.",
-                    orangeButtonText: viewModel.isSuccessGetData ? "확인" : "닫기",
-                    onTapOrangeButton: {
-                        viewModel.showPatchAlert = false
-                    }
-                )
-            }
-            
-            if viewModel.showDeleteAlert {
-                CustomedAlert(
-                    alertImage: viewModel.isSuccessGetData ? "img_fail" : "img_fail" ,
-                    titleText: viewModel.isSuccessGetData ? "모임이 삭제되었어요!" : "알 수 없는 이유로 삭제가 거부되었습니다.",
-                    subtitleText: viewModel.isSuccessGetData ? "다른 모임을 더 등록해보세요!" : nil,
-                    orangeButtonText: viewModel.isSuccessGetData ? "완료" : "닫기",
-                    onTapOrangeButton: {
-                        
-                        viewModel.showDeleteAlert = false
-                        
-                        viewModel.isSuccessGetData ? navigationManager.pop() : nil
-                        print("pop")
-                    }
-                )
-            }
-          
-            if viewModel.showFullErrorView {
-                FullErrorView(onTapRetryButton: {
-                    viewModel.showFullErrorView = false
-                    
-                    viewModel.fetchAllData(groupId: groupId, groupType: groupType)
-                })
-                .customNavigationBar(showBackButton: true)
-            }
-            
-            if viewModel.showErrorAlert {
-                CustomedAlert(
-                    alertImage: "img_fail" ,
-                    titleText: "앗! 데이터를 불러오지 못했어요.",
-                    subtitleText: "다시 시도해주세요.",
-                    orangeButtonText: "확인",
-                    onTapOrangeButton: {
-                        viewModel.showErrorAlert = false
                     }
                 )
             }
         }
     }
     
-    func divider() -> some View {
-        Color.gray02.frame(height: 8)
+    @ViewBuilder
+    func cancelAlert() -> some View {
+        if viewModel.showPatchAlert {
+            CustomedAlert(
+                alertImage: viewModel.isSuccessGetData ? "img_fail" : "img_fail" ,
+                titleText: viewModel.isSuccessGetData ? "모임 신청이 취소되었어요!" : "일시적인 오류가 발생했습니다.",
+                subtitleText: viewModel.isSuccessGetData ? nil : "잠시 후 다시 시도해주세요.",
+                orangeButtonText: viewModel.isSuccessGetData ? "확인" : "닫기",
+                onTapOrangeButton: {
+                    viewModel.showPatchAlert = false
+                })
+        }
+    }
+    
+    @ViewBuilder
+    func deleteAlert() -> some View {
+        if viewModel.showDeleteAlert {
+            CustomedAlert(
+                alertImage: viewModel.isSuccessGetData ? "img_fail" : "img_fail" ,
+                titleText: viewModel.isSuccessGetData ? "모임을 삭제하시겠습니까?" : "일시적인 오류가 발생했습니다.",
+                subtitleText: viewModel.isSuccessGetData ? "삭제된 모임은 복구가 불가합니다." : "잠시 후 다시 시도해주세요.",
+                orangeButtonText: viewModel.isSuccessGetData ? "삭제하기" : "닫기",
+                onTapOrangeButton: {
+                    
+                    viewModel.showDeleteAlert = false
+                    
+                    viewModel.isSuccessGetData ? navigationManager.pop() : nil
+                    print("pop")
+                })
+        }
+    }
+    
+    @ViewBuilder
+    func defaultFullErrorView() -> some View {
+        if viewModel.showFullErrorView {
+            FullErrorView(onTapRetryButton: {
+                viewModel.showFullErrorView = false
+                
+                viewModel.fetchAllData(groupId: groupId, groupType: groupType)
+            })
+            .customNavigationBar(showBackButton: true)
+        }
+    }
+    
+    @ViewBuilder
+    func defaultErrorAlert() -> some View {
+        if viewModel.showErrorAlert {
+            CustomedAlert(
+                alertImage: "img_fail" ,
+                titleText: "앗! 데이터를 불러오지 못했어요.",
+                subtitleText: "다시 시도해주세요.",
+                orangeButtonText: "확인",
+                onTapOrangeButton: {
+                    viewModel.showErrorAlert = false
+                }
+            )
+        }
     }
 }
